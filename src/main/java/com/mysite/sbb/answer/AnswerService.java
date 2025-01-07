df0518fa2a4ff82f150ba2820dd3cb4d84215571
gorhkd <gorhkd223@gmail.com>
2025-01-07 12:31:54 +0900

    3-8

--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -1,6 +1,8 @@
 package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +13,16 @@ import java.time.LocalDateTime;
 public class AnswerService {
 
     private final AnswerRepository answerRepository;
+    private final UserService userService;
 
-    public void create(Question question, String content) {
+    public void create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
+        answer.setAuthor(author);
         this.answerRepository.save(answer);
     }
+
+
 }
